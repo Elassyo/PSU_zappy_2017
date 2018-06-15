5feb42cbe43ca933ba937778800782d88b7a9b2c
faudil.puttilli <faudil.puttilli@epitech.eu>
2018-06-15 19:15:25 +0200

    Changes in CMakeList.txt

--- src/ai/Drone.hpp
+++ src/ai/Drone.hpp
@@ -8,4 +8,13 @@
 #ifndef PSU_ZAPPY_2017_DRONE_HPP
 #define PSU_ZAPPY_2017_DRONE_HPP
 
+namespace zappy {
+	namespace ai {
+		class Drone {
+		public:
+			Drone();
+		};
+	}
+}
+
 #endif /* PSU_ZAPPY_2017_DRONE_HPP */
