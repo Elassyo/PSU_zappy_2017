868176df78ece2e19de1d25d8a1361407f1beed8
JakaResse <jeremy.bouton@epitech.eu>
2018-06-17 16:54:18 +0200

    RequestConstructor for client commands done

--- src/ai/RequestConstructor.hpp
+++ src/ai/RequestConstructor.hpp
@@ -8,10 +8,37 @@
 #ifndef PSU_ZAPPY_2017_REQUESTCONSTRUCTOR_HPP
 #define PSU_ZAPPY_2017_REQUESTCONSTRUCTOR_HPP
 
-namespace zappy {
-	class RequestConstructor {
+#include <iostream>
+#include <map>
+#include "Inventory.hpp"
 
+namespace zappy
+{
+	class RequestConstructor
+	{
+  	public:
+		RequestConstructor();
+		~RequestConstructor() = default;
+
+		std::string moveForward();
+		std::string turnLeft();
+		std::string turnRight();
+
+		std::string look();
+		std::string inventory();
+		std::string broadcast(std::string text);
+
+		std::string connectNbr();
+		std::string fork();
+		std::string eject();
+
+		std::string takeObject(zappy::ai::Item item);
+		std::string setObject(zappy::ai::Item item);
+		std::string incantation();
+
+	private:
+		std::map<zappy::ai::Item, std::string> _itemStr;
 	};
-}
+} // namespace zappy
 
 #endif /* PSU_ZAPPY_2017_REQUESTCONSTRUCTOR_HPP */
