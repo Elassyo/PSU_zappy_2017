289a5e447fa4b657e35fa4d70e2772810b4ad1bb
faudil.puttilli <faudil.puttilli@epitech.eu>
2018-06-18 17:44:06 +0200

    Stupid warning fixed

--- src/ai/CBuffer.cpp
+++ src/ai/CBuffer.cpp
@@ -22,7 +22,7 @@ std::string zappy::CBuffer::read(const size_t n)
 {
 	char *buffer = new char[n + 1];
 
-	if (cbuf_read(&_cb, (void *)buffer, n) == -1)
+	if (cbuf_read(&_cb, (void *)buffer, n) == 0)
 		throw zappy::Exception("CBuffer", "Can't read cbuf");
 	std::string s(buffer);
 	delete [] buffer;
@@ -31,6 +31,6 @@ std::string zappy::CBuffer::read(const size_t n)
 
 void zappy::CBuffer::write(const std::string &toWrite)
 {
-	if (cbuf_write(&_cb, (void*)toWrite.c_str(), toWrite.length()) == -1)
+	if (cbuf_write(&_cb, (void*)toWrite.c_str(), toWrite.length()) == 0)
 		throw zappy::Exception("CBuffer", "Can't write cbuf");
 }
