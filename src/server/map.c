6bde144681c00fedb2b660fbc56d06f9a240c520
alexandre <alexandre.debeaumont@epitech.eu>
2018-06-18 17:00:50 +0200

    Added map generation

--- src/server/map.c
+++ src/server/map.c
@@ -6,6 +6,7 @@
 */
 
 #include <stddef.h>
+#include <stdlib.h>
 
 #include "zappy_server.h"
 
@@ -22,4 +23,24 @@ void zpy_srv_map_cleanup(zpy_srv_map_t *map)
 {
 	list_destroy(map->players);
 	list_destroy(map->items);
+}
+
+void zpy_srv_map_fill(zpy_srv_t *srv)
+{
+	unsigned int res_amt;
+	int x;
+	int y;
+	zpy_item_type_t resource;
+
+	res_amt = 5 * srv->max_clients + ((rand() % 2 * srv->max_clients) -
+		srv->max_clients);
+	for (int i = 0; i < res_amt; i++) {
+		x = rand() % srv->map.width;
+		y = rand() % srv->map.height;
+		resource = (zpy_item_type_t) (rand() % (NITEM_TYPES - 1));
+		zpy_srv_map_add_item(srv->map, x, y, resource);
+		x = rand() % srv->map.width;
+		y = rand() % srv->map.height;
+		zpy_srv_map_add_item(srv->map, x, y, FOOD);
+	}
 }
