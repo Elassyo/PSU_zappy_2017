6a1e1bc4971347f5c55031a96b559aa8409fbe42
JakaResse <jeremy.bouton@epitech.eu>
2018-06-19 14:55:34 +0200

    Enum Item add DRONE in Inventory, Vision and Tile classes for parsing look command

--- src/ai/Tile.hpp
+++ src/ai/Tile.hpp
@@ -1,24 +1,37 @@
-//
-// EPITECH PROJECT, 2018
-// PSU_zappy_2017
-// File description:
-// Tile.hpp
-//
+/*
+** EPITECH PROJECT, 2018
+** PSU_zappy_2017
+** File description:
+** Tile.hpp
+*/
 
 #ifndef PSU_ZAPPY_2017_TILE_HPP
-#define PSU_ZAPPY_2017_TILE_HPP
+	#define PSU_ZAPPY_2017_TILE_HPP
 
-namespace zappy
-{
-	namespace ai
-	{
-		class Tile
-		{
+	#include <sstream>
+	#include <iostream>
+	#include "Inventory.hpp"
+#include "Vertex.hpp"
+#include "Drone.hpp"
+
+
+namespace zappy {
+	namespace ai {
+		class Tile {
 		public:
-			Tile();
+			Tile(const std::string &itTiles);
 			~Tile() = default;
+
+			VertexS computePosition
+				(const VertexS &playerPos,
+				 const VertexS &mapSize, ai::Direction) const;
+
+		private:
+			std::map<std::string, zappy::ai::Item> _strItem;
+			std::vector<zappy::ai::Item> _tileItems;
 		};
 	}
 }
+
 
 #endif /* PSU_ZAPPY_2017_TILE_HPP */
--- src/ai/Vision.hpp
+++ src/ai/Vision.hpp
@@ -1,24 +1,24 @@
-//
-// EPITECH PROJECT, 2018
-// PSU_zappy_2017
-// File description:
-// Vision.hpp
-//
+/*
+** EPITECH PROJECT, 2018
+** PSU_zappy_2017
+** File description:
+** Vision.hpp
+*/
 
 #ifndef PSU_ZAPPY_2017_VISION_HPP
 	#define PSU_ZAPPY_2017_VISION_HPP
 
-	#include <string>
+	#include <iostream>
 	#include <vector>
+	#include <sstream>
 	#include "Tile.hpp"
+	#include "Exception/Exception.hpp"
 
-namespace zappy
-{
+namespace zappy {
 	namespace ai {
 		class Vision {
 		public:
 			Vision(const std::string &);
-
 			~Vision() = default;
 
 		private:
