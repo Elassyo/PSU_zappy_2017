0d274b247d59416c649edae4f17bcb42e2083872
Nico <nicolas.lemaire@epitech.eu>
2018-06-19 12:20:42 +0200

    Add inventory and bct cmd

--- src/server/cmd_ai/cmd_inventory.c
+++ src/server/cmd_ai/cmd_inventory.c
@@ -12,8 +12,13 @@ bool cmd_inventory(tcp_conn_t *conn,
 		__attribute__((unused))zpy_srv_client_t *client,
 		__attribute__((unused))char const *cmd)
 {
-	printf("inventory\n");
-	//TODO
+	zpy_srv_player_t *player = client->player;
+	char *items[NITEM_TYPES] = {"food", "linemate", "deraumere",
+				"sibur", "mendiane", "phiras", "thystame"};
+
+	for (unsigned short i = 0; i < NITEM_TYPES; i++) {
+		printf("%s : %d", items[i], player->inventory[i]);
+	}
 	cbuf_write(&conn->out, "todo\n", 5);
 	return (true);
 }
--- src/server/cmd_graphic/cmd_map.c
+++ src/server/cmd_graphic/cmd_map.c
@@ -23,12 +23,24 @@ bool cmd_msz(tcp_conn_t *conn,
 
 bool cmd_bct(tcp_conn_t *conn,
 		zpy_srv_client_t *client,
-		__attribute__((unused))char const *cmd)
+		char const *cmd)
 {
-	char buff[256];
+	/* int x = -1; */
+	/* int y = -1; */
+	/* char count[7]; */
+	/* zpy_srv_item_group_t *obj; */
 
-	snprintf(buff, 256, "msz %u %u\n", client->server->map.width,
-		client->server->map.height);
-	cbuf_write(&conn->out, &buff, strlen(buff));
+	/* memset(count, 0, 7); */
+	/* sscanf(cmd, "%d %d", &x, &y); */
+	/* if (x == -1 || y == -1) { */
+	/* 	cbuf_write(&conn->out, "ko\n", 3); */
+	/* 	return (true); */
+	/* } */
+	/* for (list_node_t *curr = client->server->map.items->head; curr != NULL; curr = curr->next) { */
+	/* 	obj = (zpy_srv_item_group_t *)curr->data; */
+	/* 	printf("%u %u\n", obj->x, obj->y); */
+	/* } */
+	/* /\* printf("%s et %d %d\n", cmd, x, y); *\/ */
+	/* cbuf_write(&conn->out, cmd, strlen(cmd)); */
 	return (true);
 }
