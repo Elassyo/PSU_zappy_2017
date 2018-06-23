fa792c77bebbf365b87166240bec10cd7d9c3090
Arthur Melin <arthur.melin@epitech.eu>
2018-06-23 18:41:44 +0200

    Reality check by C : explicitly call va_arg in the right order for GC messages

--- src/server/graphic/grph_pic.c
+++ src/server/graphic/grph_pic.c
@@ -9,11 +9,12 @@
 
 void zpy_srv_grph_pic(tcp_conn_t *conn, va_list args)
 {
+	zpy_srv_client_t *client = va_arg(args, zpy_srv_client_t*);
 	list_t *players;
 	list_node_t *node;
 	zpy_srv_player_t *player;
 
-	players = zpy_srv_incantation_players(va_arg(args, zpy_srv_client_t*));
+	players = zpy_srv_incantation_players(client);
 	node = players->head;
 	player = node->data;
 	tcp_conn_printf(conn, "pic %u %u %u",
